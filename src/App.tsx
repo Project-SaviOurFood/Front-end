d5fa0863bab45b5df45afe429d1e1262fbcf2b89
Edevando Alves <edevando.xavier@gmail.com>
2023-10-24 00:29:41 -0300

    feat: category done

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
-import { Route, Routes } from 'react-router-dom';
-import Register from './pages/Register/Register';
-import Login from './pages/Login';
-import Product from './pages/Product/Product';
-import Footer from './components/Footer';
-import Home from './pages/Home/Home';
-import Cart from './pages/Cart/Cart';
-import PurchaseSuccess from './pages/ PurchaseSuccess';
-import Category from './pages/Categories/Category';
-import FormCategory from './pages/Categories/FormCategory';
-import DeleteCategory from './pages/Categories/DeleteCategory';
-import FormProduct from './pages/Product/formproduct/FormProduct';
-import DeletarProduct from './pages/Product/deleteproduct/DeleterProduct';
-
-
+import { Route, Routes } from "react-router-dom";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login";
+import Product from "./pages/Product/Product";
+import Footer from "./components/Footer";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PurchaseSuccess from "./pages/ PurchaseSuccess";
+import Category from "./pages/Categories/Category";
+import FormCategory from "./pages/Categories/FormCategory";
+import DeleteCategory from "./pages/Categories/DeleteCategory";
+import FormProduct from "./pages/Product/formproduct/FormProduct";
+import DeletarProduct from "./pages/Product/deleteproduct/DeleterProduct";
 
 function App() {
   return (
     <>
-    <Routes>
-    <Route path='/' element={<Home/>} />
-    <Route path='/login' element={<Login/>} />
-    <Route path='/register' element={<Register/>} />
-    <Route path='/categories' element={<Category/>} />
-    <Route path='/registerCategory' element={<FormCategory/>} />
-    <Route path='/editCategory/:id' element={<FormCategory/>} />
-    <Route path='/deleteCategory/:id' element={<DeleteCategory/>} />
-    <Route path='/products' element={<Product/>} />
-    <Route path='/registerProduct' element={<FormProduct/>} />
-    <Route path='/editProduct/:id' element={<FormProduct/>} />
-    <Route path='/deleteProduct/:id' element={<DeletarProduct/>} />
-    <Route path='/cart' element={<Cart/>} />
-    <Route path='/finish' element={<PurchaseSuccess/>} />
-    </Routes>
-    <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/categories" element={<Category />} />
+        <Route path="/registerCategory" element={<FormCategory />} />
+        <Route path="/editCategory/:id" element={<Category />} />
+        <Route path="/deleteCategory/:id" element={<DeleteCategory />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/registerProduct" element={<FormProduct />} />
+        <Route path="/editProduct/:id" element={<FormProduct />} />
+        <Route path="/deleteProduct/:id" element={<DeletarProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/finish" element={<PurchaseSuccess />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Categories/FormCategory.tsx
+++ src/pages/Categories/FormCategory.tsx
@@ -1,9 +1,10 @@
-import { ChangeEvent, useContext, useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { put, get, register } from '../../service/Service';
-import { toastAlerta } from '../../utils/toastAlert';
-import { UserContext } from '../../context/UserContext';
-import ICategory from '../../interfaces/ICategory';
+import { ChangeEvent, useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { put, get, register } from "../../service/Service";
+import { toastAlerta } from "../../utils/toastAlert";
+import { UserContext } from "../../context/UserContext";
+import ICategory from "../../interfaces/ICategory";
+import "../../components/category/Category.css"
 
 export default function FormCategory() {
   const [category, setCategory] = useState<ICategory>({} as ICategory);
@@ -12,7 +13,10 @@ export default function FormCategory() {
 
   const { id } = useParams<{ id: string }>();
 
-  const { userResponse: {token}, handleLogout } = useContext(UserContext);
+  const {
+    userResponse: { token },
+    handleLogout,
+  } = useContext(UserContext);
 
   async function findById(id: string) {
     await get(`/category/${id}`, setCategory, {
@@ -24,100 +28,102 @@ export default function FormCategory() {
 
   useEffect(() => {
     if (id !== undefined) {
-      findById(id)
+      findById(id);
     }
-  }, [id])
+  }, [id]);
 
   function updateState(e: ChangeEvent<HTMLInputElement>) {
     setCategory({
       ...category,
-      [e.target.name]: e.target.value
-    })
+      [e.target.name]: e.target.value,
+    });
 
-    console.log(JSON.stringify(category))
+    console.log(JSON.stringify(category));
   }
 
   async function newCategory(e: ChangeEvent<HTMLFormElement>) {
-    e.preventDefault()
+    e.preventDefault();
 
     if (id !== undefined) {
       try {
         await put(`/category`, category, setCategory, {
           headers: {
-            'Authorization': token
-          }
-        })
+            Authorization: token,
+          },
+        });
 
-        toastAlerta('Category atualizada com sucesso', 'sucesso')
-        navigate('/categories')
-
+        toastAlerta("Category atualizada com sucesso", "sucesso");
+        navigate("/categories");
       } catch (error: any) {
-        if (error.toString().includes('403')) {
-          toastAlerta('O token expirou, favor logar novamente', 'info')
-          handleLogout()
+        if (error.toString().includes("403")) {
+          toastAlerta("O token expirou, favor logar novamente", "info");
+          handleLogout();
         } else {
-          toastAlerta('Erro ao atualizar a Category', 'erro')
+          toastAlerta("Erro ao atualizar a Category", "erro");
         }
-
       }
-
     } else {
       try {
         await register(`/category`, category, setCategory, {
           headers: {
-            'Authorization': token
-          }
-        })
+            Authorization: token,
+          },
+        });
 
-        toastAlerta('Category cadastrada com sucesso', 'sucesso')
-
+        toastAlerta("Category cadastrada com sucesso", "sucesso");
       } catch (error: any) {
-        if (error.toString().includes('403')) {
-          toastAlerta('O token expirou, favor logar novamente', 'info')
-          handleLogout()
+        if (error.toString().includes("403")) {
+          toastAlerta("O token expirou, favor logar novamente", "info");
+          handleLogout();
         } else {
-          toastAlerta('Erro ao cadastrar a Category', 'erro')
+          toastAlerta("Erro ao cadastrar a Category", "erro");
         }
       }
     }
 
-    navigate('/categories')
+    navigate("/categories");
   }
 
   useEffect(() => {
-    if (token === '') {
-      toastAlerta('Você precisa estar logado', 'info');
-      navigate('/login');
+    if (token === "") {
+      toastAlerta("Você precisa estar logado", "info");
+      navigate("/login");
     }
   }, [token]);
 
   return (
     <div>
-      <h1>
-        {id === undefined ? 'Cadastre uma nova Categoria' : 'Editar Categoria'}
+      <h1 className="flex justify-center font-bold text-lg">
+        {id === undefined ? "Cadastre uma nova categoria" : "Editar Categoria"}
       </h1>
       <form onSubmit={newCategory}>
-        <div>
-          <label htmlFor="type">Tipo:</label>
+        <div className="flex flex-col sidelabel">
+          <label htmlFor="type">Categoria:</label>
           <input
-              type="text"
-              placeholder="Tipo"
-              name='type'
-              value={category.type}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => updateState(e)} />
-          <label htmlFor="descricao">Descrição da Categoria:</label>
-          <input
+          className="w-full"
             type="text"
-            placeholder="Descrição"
-            name='description'
+            placeholder="Ex: Grãos"
+            name="type"
+            value={category.type}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => updateState(e)}
+          />
+          <label htmlFor="descricao" className="mt-4">
+            Descrição da Categoria:
+          </label>
+          <input
+            className="w-full"
+            type="text"
+            placeholder="Ex: Grãos ricos em carboidratos..."
+            name="description"
             value={category.description}
             onChange={(e: ChangeEvent<HTMLInputElement>) => updateState(e)}
           />
         </div>
         <button
           type="submit"
+          className="bg-vermelho p-2 rounded-lg mt-10 text-amarelo font-bold"
         >
-          {id === undefined ? 'Cadastrar' : 'Editar'}
+          {id === undefined ? "Cadastrar" : "Editar"}
         </button>
       </form>
     </div>
